188c4e20b4298f14e48561848415ffc2fc294abd
Youssef Emad <144681315+Youssef389@users.noreply.github.com>
2024-11-19 23:57:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+    });
+
+    $(window).on('scroll load', function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+
+        if($(window).scrollTop() > 60){
+            $('.header').addClass('active');
+        }
+        else{
+            $('.header').removeClass('active');
+        }
+
+        $('section').each(function(){
+
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+
+        });
+
+    });
+
+})
+
+let scrollAmount = 0;
+
+function scrollCarousel(direction) {
+  const container = document.querySelector(".carousel__container");
+  const itemWidth = container.querySelector(".carousel-item").offsetWidth + 10; 
+  const visibleWidth = container.parentElement.offsetWidth;
+
+  scrollAmount += direction * itemWidth;
+
+
+  const maxScroll = container.scrollWidth - visibleWidth;
+  if (scrollAmount < 0) scrollAmount = 0;
+  if (scrollAmount > maxScroll) scrollAmount = maxScroll;
+
+  container.style.transform = `translateX(-${scrollAmount}px)`;
+}
+let guestCount = 1;
+const guestDisplay = document.getElementById('guest-count');
+const decreaseBtn = document.getElementById('decrease');
+const increaseBtn = document.getElementById('increase');
+
+decreaseBtn.addEventListener('click', () => {
+  if (guestCount > 1) {
+    guestCount--;
+    guestDisplay.innerText = `Guests: ${guestCount}`;
+  }
+});
+
+increaseBtn.addEventListener('click', () => {
+  guestCount++;
+  guestDisplay.innerText = `Guests: ${guestCount}`;
+});
+
+let roomCount = 1;
+const roomDisplay = document.getElementById('room-count');
+const decreasebtn = document.getElementById('decreases');
+const increasebtn = document.getElementById('increases');
+
+decreasebtn.addEventListener('click', () => {
+  if (roomCount > 1) {
+    roomCount--;
+    roomDisplay.innerText = `Rooms: ${roomCount}`;
+  }
+});
+
+increasebtn.addEventListener('click', () => {
+  roomCount++;
+  roomDisplay.innerText = `Rooms: ${roomCount}`;
+});
+
